0f223a9052f7ef02a58a927cd40957338d54ae1f
Brendon-MxRp <brendonmax7@gmail.com>
2022-04-06 09:31:00 +0200

    Made a simple madlib

--- Madlib.py
+++ Madlib.py
@@ -1,1 +1,7 @@
-print('hello world')
+adj = input("Adjective: ")
+emotion = input("Emotion: ")
+verb1 = input("Verb: ")
+noun1 = input("Noun: ")
+madlib = f"The sun is {adj}! That makes me feel {emotion}, so i shall {verb1} the {noun1}!"
+
+print(madlib)
